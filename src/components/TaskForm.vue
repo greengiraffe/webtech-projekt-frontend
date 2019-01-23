1ba45e6acbb2a0e5e1474db8cbcae4aca8e7612c
Tamara Voigt <mail@tamara-voigt.de>
2019-01-23 15:13:50 +0100

    WIP taskform

--- src/components/TaskForm.vue
+++ src/components/TaskForm.vue
@@ -5,43 +5,55 @@
             <h1 v-else>New task</h1>
             <v-container class="taskdetails">
                 <div>
-                    <v-textarea v-if="!edit" v-model="this.task.text" label="Task text" required="required" clearable></v-textarea>
-                    <v-textarea v-else v-model="this.newtext" rows='1' label="Task text" required="required" clearable></v-textarea>
-                </div>
-                <div>
                     <v-layout wrap align-center>
                         <v-flex xs12 sm6 d-flex>
                             <v-select v-model="newtypename" :items="tasktype_names" label="Tasktype"></v-select>
                         </v-flex>
                     </v-layout>
                 </div>
-                <div>
-                    <v-text-field v-model="this.neworder" label="Order"></v-text-field>
+
+                <div v-if="newtypename === ''">
+                    <p>Please choose a task type to proceed!</p>
+
+                    <div id="newtexttask" v-if="newtypename === 'text'">
+                        <p>Please enter the text and use the Button to indicate where blanks should be set.</p>
+                        <v-btn @click="add_answer()">Blank</v-btn>
+                        <v-text-field id="blanktext" v-model="newtasktext" label="Task text" required="required" clearable></v-text-field>
+                        <div id="blankanswers" v-for="(blank, index) in answerfields" :key="index">
+                            <v-flex xs12 sm6 md3>
+                                <v-text-field :label="'Answer: '+(index+1)" :v-model="blank.text"></v-text-field></v-flex>
+                        </div>
+                    </div>
                 </div>
 
-                <div class="answers">
-                    <h1>Answers</h1>
-
-                    <v-data-table :headers="taskdetails" :items="quiztasks" class="quiztasks">
+                <div v-else>
+                    <p>Please enter a question and add a few answers.</p>
+                    <v-text-field id="question" v-model="newtasktext" label="Task text" required="required" clearable></v-text-field>
+                    <div id="choiceanswers">
+                        <h2>Answers</h2>
+                        <v-data-table :headers="headers" :items="newanswers" >
                             <template slot="headers" slot-scope="props">
                                 <tr>
-                                    <th v-for="header in taskdetails" :key="header" >{{header}}</th>
+                                    <th v-for="header in headers" :key="header" >{{header}}</th>
                                     <th><v-btn title="Add new task"><v-icon>add</v-icon></v-btn></th>
                                 </tr>
                             </template>
                             <template slot="items" slot-scope="props">
-                                <tr v-for="task in quiztasks" :key="task.id">
-                                    <td>{{task.order}}</td>
-                                    <td>{{task.text}}</td>
-                                    <td>{{task.type.name}}</td>
-                                    <td>{{task.solved}}</td>
-                                    <td>
-                                        <v-btn flat icon title="Edit this task"><v-icon>create</v-icon></v-btn>
-                                        <v-btn flat icon title="Delete this task" color="#ba1a0e"><v-icon>delete</v-icon></v-btn>
-                                    </td>
-                                </tr>
+                                <td>
+                                    <v-edit-dialog :return-value.sync="props.item.text" lazy @save="save" @cancel="cancel" @open="open" @close="close">
+                                        {{ props.item.text }}
+                                        <v-text-field slot="input" v-model="props.item.text" label="Edit" multi-line></v-text-field>
+                                    </v-edit-dialog>
+                                </td>
+                                <td id="choiceCheck">
+                                    <v-checkbox slot="input" v-model="props.item.choice" autofocus></v-checkbox>
+                                </td>
+                                <td>
+                                    <v-btn flat icon title="Delete this answer" color="#ba1a0e"><v-icon>delete</v-icon></v-btn>
+                                </td>
                             </template>
                         </v-data-table>
+                    </div>
                 </div>
             </v-container>
         </v-card>
@@ -53,8 +65,15 @@ export default {
         return {
             props: ['edit'],
             newtext: '',
-            neworder: 0,
             newtypename: '',
+            newanswers: [
+                {
+                    text: 'Answer me',
+                    choice: true
+                }
+            ],
+            newtasktext: '',
+            nrofanswers: 0,
             edit: false,
             task: {
                 id: 1,
@@ -66,15 +85,69 @@ export default {
                 { id: 2, name: 'multiple-choice' },
                 { id: 3, name: 'text' }
             ],
-            answerdetailsChoice: ['Text', 'Correct choice?'],
-            answerdetailsText: ['Text']
+            headers: ['Text', 'Correct choice?'],
         } // how to add tasks and answers in backend? Values? Connection between quiz - task - answer
         // connection between text task and text answer?
     },
     computed: {
         tasktype_names: function () {
             return this.tasktypes.map(tasktype => tasktype.name)
+        },
+        answerfields: function () {
+            return this.newanswers
+        }
+    },
+    methods: {
+        add_answer: function () {
+            this.nrofanswers++
+            var placeholder = '#ANSWER:' + this.nrofanswers + '#'
+            this.newtasktext += placeholder
+            if (this.newanswers.length < this.nrofanswers) {
+                this.newanswers.push({ text: '' })
+            }
         }
     }
 }
 </script>
+
+<style lang="scss">
+.taskform > .v-card {
+  margin: 5% auto;
+  width: 70%;
+  padding: 5% 0%;
+
+  .datainput {
+    width: 80%;
+    margin: 2% auto;
+
+    .small {
+      width: 40%;
+    }
+    > :first-child {
+      float: right;
+    }
+    .btns {
+      text-align: center;
+    }
+  }
+}
+td:first-child{
+    width: 90%;
+    text-align: left;
+}
+td{
+    text-align: center;
+}
+#choiceCheck{
+    .v-input__control{
+        margin: 0 auto;
+
+        .v-input__slot{
+            margin: 0;
+        }
+        .v-messages{
+            display: none;
+        }
+    }
+}
+</style>
